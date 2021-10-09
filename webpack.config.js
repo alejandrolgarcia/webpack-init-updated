ea44d0babe580f57f14b50322e223741ef1c410e
Luis Garcia <lalejandro.1988@gmail.com>
2021-10-08 18:27:51 -0600

    Configuracion CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     mode: 'development',
@@ -24,7 +25,11 @@ module.exports = {
             },
             {
                 test: /styles.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
             }
         ]
     },
@@ -36,10 +41,19 @@ module.exports = {
             template: './src/index.html',
             // filename: './index.html'
         }),
+
         new MiniCssExtractPlugin({
             filename: '[name].css',
             ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets/',
+                    to: 'assets/'
+                }
+            ]   
         })
     ]
-
 }
